07cac42c94b634115a61850681a1ca7c0c6c5a40
Daniel Morley <morleydan10@gmail.com>
2023-11-10 15:56:25 -0500

    added fetch in js, added script in html to connect to index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// fetch("http://localhost:3000/alphabet")
-// .then((res) => res.json())
-// .then((data) => {
-//     console.log(data)
-// });
+fetch("http://localhost:3000/alphabet")
+.then((res) => res.json())
+.then((data) => {
+    console.log(data)
+// for (i=0; i < data.length; i++);
+});
+
